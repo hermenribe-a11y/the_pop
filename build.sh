030c6aee50785d352350f01dff2d7981942acc87
hermenribe-a11y <hermenribe@gmail.com>
2026-05-22 13:22:09 +0200

    Initial commit

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@
 set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
- manage.py collectstatic --noinput
- manage.py migrate
+python manage.py collectstatic --noinput
+python manage.py migrate
